cb39e954a5c817b335d93bbddae664070b3ff6d1
Theerawat <bsamui456@gmail.com>
2026-02-23 14:56:11 +0700

    Revert "url password.root"
    This reverts commit bac6bc8aba018cdb2ac9e4cfe96f9008bbf18ce7.

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    # พิมพ์บรรทัดนี้ลงไปแทนของเดิมครับ
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost/ecommerce_db"
+    SQLALCHEMY_DATABASE_URI = \
+        "mysql+pymysql://root@localhost/ecommerce_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "secret-key"
